9575cd4c1a1417ce8806ad5f645c454e93bc7c8d
dan-antonelli <danielantonelli.personal@gmail.com>
2025-03-29 00:08:26 +0100

    style navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="flex flex-col">
+      <div className="flex flex-gap-4 mb-6">
+        <a href="#" className="a-btn font-md p-1">
+          projects
+        </a>
+        <a href="#" className="a-btn font-md p-1">
+          blog
+        </a>
+        <a href="#" className="a-btn font-md p-1">
+          about
+        </a>
+      </div>
       <div className="text-center">
         <h1>Daniel Antonelli</h1>
       </div>
