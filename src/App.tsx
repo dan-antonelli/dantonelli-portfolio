c5f7ddc038dfbda188fece039359129e473e06dc
dan-antonelli <danielantonelli.personal@gmail.com>
2025-03-29 12:46:25 +0100

    center container

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex flex-col">
-      <nav className="flex flex-jc-sb flex-ai-center mt-1 mb-6">
+    <div className="container">
+      <nav className="flex flex-jc-sb flex-ai-center mt-1 mb-6 w-100">
         <img src="/home.png" alt="home" />
         <div className="flex flex-gap-1 fit-content">
-          <a href="#" className="a-btn font-md p-1">
+          <a href="#" className="a-btn font-md p-025">
             projects
           </a>
-          <a href="#" className="a-btn font-md p-1">
+          <a href="#" className="a-btn font-md p-025">
             blog
           </a>
-          <a href="#" className="a-btn font-md p-1">
+          <a href="#" className="a-btn font-md p-025">
             about
           </a>
         </div>
