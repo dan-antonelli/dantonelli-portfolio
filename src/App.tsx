f9e36af90e32f8cc03899f27897549c778475a85
dan-antonelli <danielantonelli.personal@gmail.com>
2025-03-29 14:11:33 +0100

    wip add background shapes

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import Background from './Background';
 
 function App() {
   return (
-    <div className="container">
+    <>
       <Background />
-      <nav>
-        <a href="#" className="home">
-          <img src="/home.png" alt="home" />
-        </a>
-        <div className="nav-link-container">
-          <a href="#">about</a>
-          <a href="#">projects</a>
-          <a href="#">blog</a>
+      <div className="container">
+        <nav>
+          <a href="#" className="home">
+            <img src="/home.png" alt="home" />
+          </a>
+          <div className="nav-link-container">
+            <a href="#">about</a>
+            <a href="#">projects</a>
+            <a href="#">blog</a>
+          </div>
+        </nav>
+        <div className="title-container">
+          <h1>Daniel Antonelli</h1>
         </div>
-      </nav>
-      <div className="title-container">
-        <h1>Daniel Antonelli</h1>
       </div>
-    </div>
+    </>
   );
 }
 
