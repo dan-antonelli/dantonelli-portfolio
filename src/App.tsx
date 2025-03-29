f505ce0d2ceb177d433f1bf4d155cc1c1d542a0e
dan-antonelli <danielantonelli.personal@gmail.com>
2025-03-29 11:41:05 +0100

    working on navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-gap-1 mb-6">
+      <nav className="flex flex-jc-sb flex-ai-center mt-1 mb-6">
         <img src="/home.png" alt="home" />
-        <a href="#" className="a-btn font-md p-1">
-          projects
-        </a>
-        <a href="#" className="a-btn font-md p-1">
-          blog
-        </a>
-        <a href="#" className="a-btn font-md p-1">
-          about
-        </a>
-      </div>
+        <div className="flex flex-gap-1 fit-content">
+          <a href="#" className="a-btn font-md p-1">
+            projects
+          </a>
+          <a href="#" className="a-btn font-md p-1">
+            blog
+          </a>
+          <a href="#" className="a-btn font-md p-1">
+            about
+          </a>
+        </div>
+      </nav>
       <div className="text-center">
         <h1>Daniel Antonelli</h1>
       </div>
