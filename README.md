e11fe3fcbe4ac7667c5cd45fc342064a21d3db96
dan-antonelli <danielantonelli.personal@gmail.com>
2025-03-30 20:31:21 +0200

    styling project cards

--- README.md
+++ README.md
@@ -13,6 +13,11 @@
 - dark mode
 - util components for links
 - hook for several things, like copied state
+- change dvs to articles etc
 - improve file paths
 - writings page, add it to info
 - FIX BACKGROUND
+- experiment with styled components and stuff
+- switch to css in js/styled components
+- rewrite to Angular
+- add backend
--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -5,6 +5,13 @@ export default function Main() {
   return (
     <main className="container">
       <Hero />
+      <section className="projects">
+        <h2>Recent projects</h2>
+        <article className="project">
+          <h3>Webshop</h3>
+          <p>A webshop created with React.</p>
+        </article>
+      </section>
       <Contact />
     </main>
   );
