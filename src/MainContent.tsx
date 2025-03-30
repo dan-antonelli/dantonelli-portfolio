dd5186ca44d8de28f555870cf0b9cac83bbe3615
dan-antonelli <danielantonelli.personal@gmail.com>
2025-03-30 22:38:34 +0200

    add links to project

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -7,23 +7,37 @@ export default function Main() {
       <Hero />
       <section className="projects">
         <h2>Recent projects</h2>
-        <a
-          href=""
-          //   target="_blank"
-          rel="noopener noreferrer"
-          title="webshop project"
-          className="project-anchor"
-        >
-          <article className="project">
-            <div className="project-title">
-              <h3>Webshop</h3>
-            </div>
-            <div className="project-image-container">
-              <img src="/webshop.png" alt="webshop project" className="project-image" />
-            </div>
+        <article className="project">
+          <div className="project-title">
+            <h3>Webshop</h3>
+          </div>
+          <div className="project-image-container">
+            <img src="/webshop.png" alt="webshop project" className="project-image" />
+          </div>
+          <div className="project-description-container">
             <p className="project-description">A webshop created with React.</p>
-          </article>
-        </a>
+            <div className="project-links">
+              <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="github"
+                className="project-anchor"
+              >
+                project site
+              </a>
+              <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="github"
+                className="project-anchor"
+              >
+                github
+              </a>
+            </div>
+          </div>
+        </article>
       </section>
       <Contact />
     </main>
