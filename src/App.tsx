7a36f50db76cb4ab3dffd0daec86fddd7b4932de
dan-antonelli <danielantonelli.personal@gmail.com>
2025-04-01 13:57:34 +0200

    add HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-import { ReactNode } from 'react';
 import './App.css';
-import Background from './Background';
-import Navbar from './Navbar';
-import Main from './MainContent';
-import Footer from './Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PageNotFoundPage from './pages/PageNotFoundPage';
-interface DefaultLayoutProps {
-  children: ReactNode;
-}
-
-const DefaultLayout = ({ children }: DefaultLayoutProps) => (
-  <>
-    <Background />
-    <Navbar />
-    {children}
-    <Footer />
-  </>
-);
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <DefaultLayout>
-              <Main />
-            </DefaultLayout>
-          }
-        />
+        <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<PageNotFoundPage />} />
         <Route path="/about" element={<PageNotFoundPage />} />
         <Route path="*" element={<PageNotFoundPage />} />
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import Background from '../Background';
+import Footer from '../Footer';
+import Main from '../MainContent';
+import Navbar from '../Navbar';
+
+export default function HomePage() {
+  return (
+    <>
+      <Background />
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
