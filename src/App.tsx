92fe21711e1012bba8832b2ce84272390a120f1f
dan-antonelli <danielantonelli.personal@gmail.com>
2025-04-05 19:21:07 +0200

    scroll to top when navigating btw pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { HomePage, ProjectsPage, PageNotFoundPage, CVPage } from '@pages/index';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectsPage />} />
